704d08da848644d36b1e91282fa124e90a0b94b4
kunwarsid <64632089+kunwarsid@users.noreply.github.com>
2021-03-12 10:13:57 +0530

    Create Portscanner.py

---
+++ Portscanner.py
@@ -0,0 +1,20 @@
+
+
+import socket
+import termcolor
+
+def scan(target,ports):
+        for port in range(1,ports):
+                 scan_port(target,port)
+
+def scan_port(ipaddress,port) :
+
+
+            try:
+                    sock = socket.socket()
+                    sock.connect((ipaddress,port))
+                    print("[+]port opened" + str(port))
+            except:
+                    print("[_]port closed" + str(port))
+
+targets 
